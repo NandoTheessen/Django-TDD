5ad3678e2c08b4370383d3b4a7ec46e9c0b17547
NandoTheessen <nando.theessen@gmail.com>
2018-12-09 01:25:51 +0100

    use env vars for prod settings DEBUG, ALLOWED_HOST SECRET_KEY

--- superlists/settings.py
+++ superlists/settings.py
@@ -23,10 +23,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'akumrqq7!$%6tz5!)07@mwpf&9!c7hv8yecp9z+#34)76y9ygk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    DEBUG = True
+    SECRET_KEY = 'insecure-key-for-dev'
+    ALLOWED_HOSTS = []
 
 # Application definition
 
